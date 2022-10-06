86ac71a92bf50332c57af42dbb38b9c0d41f7b65
manjeetre <manjeet.mehta04@gmail.com>
2022-10-06 23:11:36 +0530

    first react project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+
+// function FrequentlyAskedQuestion() {
+//   return (
+//     <details className="faq">
+//       <summary>hi</summary>
+//       <div className="answer">
+//         manjeet
+//       </div>
+//     </details>
+//   );
+// }
+
+
+export default function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <details className="faq">
+      <summary>hi</summary>
+      <div className="answer">
+        manjeet
+      </div>
+    </details>
+    </>
+  )
 }
 
-export default App;
+
---
+++ src/component.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+function FrequentlyAskedQuestion() {
+    return (
+      <details className="faq">
+        <summary>hi</summary>
+        <div className="answer">
+          manjeet
+        </div>
+      </details>
+    );
+  }
+
+  export default FrequentlyAskedQuestion
